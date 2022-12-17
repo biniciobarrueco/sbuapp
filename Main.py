cc44563cb45121708644b61ab289b3b83bd9971f
biniciobarrueco <biniciobarrueco@protonmail.com>
2022-12-16 22:44:11 -0300

    Se crea proyecto avanzado hasta el momento

---
+++ Main.py
@@ -0,0 +1,104 @@
+import Source_rc
+import sys
+import os
+import PySide6
+from pathlib import Path, PureWindowsPath
+from PyQt5.QtGui import QColor
+from PyQt5 import QtGui, QtCore
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import (QPropertyAnimation, QEasingCurve, Qt, QPoint,
+                          QPointF, QEvent, QSize, pyqtSignal, pyqtSlot)
+from PyQt5.QtWidgets import (QApplication, QMainWindow,
+                             QGraphicsDropShadowEffect, QPushButton, QLabel,
+                             QLayout, QLineEdit, QGraphicsView, QGraphicsScene,
+                             QGraphicsEllipseItem, QHeaderView, QWidget,
+                             QGraphicsObject, QSizeGrip, QSlider, QFrame)
+from animations_sbu import Animations
+
+
+# _____GETTING THE DIRECTORY WHERE WE STORE THE UI FILE______#
+directory = PureWindowsPath(os.path.dirname(__file__) + "\Sistema_Empresa.ui")
+file_directory = Path(directory)
+
+
+# _____________BUILTED MAIN CLASS WITH THE UI APP____________#
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi(file_directory, self)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        #--------------------defining animations--------------------#
+        # 1) widgetLft
+        self.animationLftWdgt = QPropertyAnimation(self.widgetLft,
+                                                   b'maximumWidth')
+        self.settings_animations(self.animationLftWdgt)
+
+        # 2) leftMenu
+        self.animationLftMenu = QPropertyAnimation(self.leftMenu,
+                                                   b'maximumWidth')
+        self.animationLftMenu.setDuration(400)
+        self.animationLftMenu.setEasingCurve(QEasingCurve.InQuad)
+
+        #-------Setting hiden the objects of the main window at the  begin-----#
+        self.rightMenu.hide()
+        self.stackedWidget_2.hide()
+
+        # ------Defining connections of the widgets within UI APP-----#
+        self.salirBtn.clicked.connect(self.close)
+        self.minimizarBtn.clicked.connect(self.showMinimized)
+        self.expandirBtn.clicked.connect(self.expand_mainwindow_actions)
+        self.miempresaBtn.clicked.connect(self.on_clicked)
+        
+        
+# _____CREATING THE CONNECTIONS TO THE EXTERNAL MODULES______#
+
+    def on_clicked(self):
+        Animations.toggle_left(self, self.animationLftMenu, self.leftMenu)
+        Animations.toggle_left(self, self.animationLftWdgt, self.widgetLft)
+
+# _______________DEFINING GENERAL SETTINGS___________________#
+
+    def settings_animations(self, qproperty):
+        qproperty.setDuration(600)
+        qproperty.setEasingCurve(QEasingCurve.InQuad)
+        return qproperty
+    
+# ________________DEFINING MainWindow EVENTS_____________________#    
+
+    def mousePressEvent(self, event):
+        self.oldposition = event.globalPos()
+        #---Launch event at double clicked---
+        if event.type() == QEvent.MouseButtonDblClick:
+            self.expand_mainwindow_actions()
+
+    def mouseMoveEvent(self, event):
+        if self.windowState() == Qt.WindowFullScreen:
+            self.expand_mainwindow_actions()
+
+        delta = QPoint(event.globalPos() - self.oldposition)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldposition = event.globalPos()
+
+    def expand_mainwindow_actions(self):
+        if self.windowState() == Qt.WindowFullScreen:
+            self.showNormal()
+            self.expandirBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/square.png"))
+
+        else:
+            self.showFullScreen()
+            self.expandirBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/multiple-file.png"))
+        
+
+
+# _____________________________________________________#
+# WE LAUNCH EXECUTE Qapplication WITH sys.argv TO KEEPING IN LOOP THE APP.
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    my_app = MainWindow()
+    my_app.show()
+    sys.exit(app.exec())
---
+++ animations_sbu.py
@@ -0,0 +1,29 @@
+
+from PyQt5 import QtGui
+import Source_rc
+from PyQt5.QtWidgets import QMainWindow
+
+#______ANIMATION CLASS THAT HAS THE BEHAVIOR OF THE OBJECTS_______#
+class Animations(QMainWindow):
+
+    def toggle_left(self, animation,object):
+        
+        #calculate the end postition for the menu
+        width = object.width()
+
+        if width==0:
+            #If the menu es currently hidden, show it by moving it onto the screen
+            new_width=190
+            self.miempresaBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/angle-small-left (1).png"))
+        else:
+            #If the menu es currently visible, hide it by moving it off the left edge of the screen
+            new_width=0
+            self.miempresaBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/menu-hamburguesa.png"))
+        #set the start and end values for the animation
+        animation.setStartValue(width)
+        animation.setEndValue(new_width)
+        animation.start()
+        
+        

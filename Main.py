a4538728458f1d3e38576cff56d8c7f7d84eb122
biniciobarrueco <biniciobarrueco@protonmail.com>
2023-01-12 20:13:19 -0300

    quitamos animaciones leftwidget y configuramos botones

--- Main.py
+++ Main.py
@@ -36,6 +36,7 @@ from PyQt5.QtWidgets import (
     QFrame,
 )
 from animations_sbu import Animations
+from Sistema_Empresa import Ui_MainWindow
 
 # _____GETTING THE DIRECTORY WHERE WE STORE THE UI FILE______#
 directory = PureWindowsPath(os.path.dirname(__file__) + "\Sistema_Empresa.ui")
@@ -43,13 +44,13 @@ file_directory = Path(directory)
 
 
 # _____________MAIN CLASS BUILTED____________#
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi(file_directory, self)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-
         # --------------------defining animations--------------------#
         # 1) widgetLft
         self.animLftWdgt = QPropertyAnimation(self.widgetLft, b"maximumWidth")
@@ -59,42 +60,6 @@ class MainWindow(QMainWindow):
         self.animLftMenu = QPropertyAnimation(self.leftMenu, b"maximumWidth")
         self.animLftMenu.setDuration(400)
         self.animLftMenu.setEasingCurve(QEasingCurve.InQuad)
-
-        # 3) mainBodyContent pages
-        self.animDBproyectos = QPropertyAnimation(self.pageDBproyectos, b"maximumWidth")
-        self.settings_animations(self.animDBproyectos)
-
-        self.animDBtrabajadores = QPropertyAnimation(
-            self.pageDBtrabajadores, b"maximumWidth"
-        )
-        self.settings_animations(self.animDBtrabajadores)
-
-        self.animDBfacturas = QPropertyAnimation(self.pageDBfacturas, b"maximumWidth")
-        self.settings_animations(self.animDBfacturas)
-
-        self.animDBprevisionales = QPropertyAnimation(
-            self.pageDBprevisionales, b"maximumWidth"
-        )
-        self.settings_animations(self.animDBprevisionales)
-
-        # 4) rightMenu widgets
-        self.animEdtProyectos = QPropertyAnimation(self.editProyectos, b"maximumWidth")
-        self.settings_animations(self.animEdtProyectos)
-
-        self.animEdtTrabajadores = QPropertyAnimation(
-            self.editTrabajadores, b"maximumWidth"
-        )
-        self.settings_animations(self.animEdtTrabajadores)
-
-        self.animEdtFacturas = QPropertyAnimation(
-            self.editPrevisionales, b"maximumWidth"
-        )
-        self.settings_animations(self.animEdtFacturas)
-
-        self.animEdtPrevisionales = QPropertyAnimation(
-            self.editFacturas, b"maximumWidth"
-        )
-        self.settings_animations(self.animEdtPrevisionales)
 
         # ------Defining connections of the widgets-----#
         self.salirBtn.clicked.connect(self.close)
@@ -106,56 +71,36 @@ class MainWindow(QMainWindow):
         self.previsionalesBtn.clicked.connect(lambda: self.on_clicked(4))
         self.trabajadoresBtn.clicked.connect(lambda: self.on_clicked(5))
 
-    # _____CREATING THE CONNECTIONS TO THE EXTERNAL MODULES______#
+        # _____CREATING THE CONNECTIONS TO THE EXTERNAL MODULES______#
 
     def on_clicked(self, connection_number):
+        mb_width = self.stackedWidget_2.width()
+        stack_widht = self.stackedWidget.width()
 
         if connection_number == 1:
+            if self.stackedWidget_2.currentWidget().width() != 0:
+                self.stackedWidget.currentWidget().setMaximumWidth(16777215)
+                self.stackedWidget_2.currentWidget().setMaximumWidth(16777215)
             Animations.toggle_left(self, self.animLftMenu, self.leftMenu)
             Animations.toggle_left(self, self.animLftWdgt, self.widgetLft)
 
         elif connection_number == 2:
-            #We can do this in a toggle function to pass only a fixed width
-            #widthDB=self.pageDBproyectos.width()
-            #widthEdt=self.editProyectos.width()
-            #if (widthDB and widthEdt) ==0:
-            #    self.pageDBproyectos.setFixedWidth(800)
-            #    self.editProyectos.setFixedWidth(350)
-                #self.stackedWidget_2.setCurrentWidget(obj2)
-                #self.stackedWidget.setCurrentWidget(obj1)
-            Animations.toggle_right(
-                self, 
-                self.animEdtProyectos,
-                self.animDBproyectos, 
-                self.editProyectos, 
-                self.pageDBproyectos
-            )
+            Animations.toggle_right(self, self.editProyectos,
+                                    self.pageDBproyectos, mb_width,
+                                    stack_widht)
 
         elif connection_number == 3:
-            Animations.toggle_right(
-                self, 
-                self.animEdtFacturas, 
-                self.animDBfacturas, 
-                self.editFacturas, 
-                self.pageDBfacturas
-            )
+            Animations.toggle_right(self, self.editFacturas,
+                                    self.pageDBfacturas, mb_width, stack_widht)
 
         elif connection_number == 4:
-            Animations.toggle_right(
-                self,
-                self.animEdtPrevisionales,
-                self.animDBprevisionales,
-                self.editPrevisionales,
-                self.pageDBprevisionales,
-            )
+            Animations.toggle_right(self, self.editPrevisionales,
+                                    self.pageDBprevisionales, mb_width,
+                                    stack_widht)
         else:
-            Animations.toggle_right(
-                self,
-                self.animEdtTrabajadores,
-                self.animDBtrabajadores,
-                self.editTrabajadores,
-                self.pageDBtrabajadores,
-            )
+            Animations.toggle_right(self, self.editTrabajadores,
+                                    self.pageDBtrabajadores, mb_width,
+                                    stack_widht)
 
     # _______________DEFINING GENERAL SETTINGS___________________#
 
@@ -183,13 +128,20 @@ class MainWindow(QMainWindow):
         self.oldposition = event.globalPos()
 
     def expand_mainwindow_actions(self):
+
+        if self.stackedWidget_2.currentWidget().width() != 0:
+            self.stackedWidget.currentWidget().setMaximumWidth(16777215)
+            self.stackedWidget_2.currentWidget().setMaximumWidth(16777215)
+
         if self.windowState() == Qt.WindowFullScreen:
             self.showNormal()
-            self.expandirBtn.setIcon(QtGui.QIcon(":/newPrefix/ICONS/square.png"))
+            self.expandirBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/square.png"))
 
         else:
             self.showFullScreen()
-            self.expandirBtn.setIcon(QtGui.QIcon(":/newPrefix/ICONS/multiple-file.png"))
+            self.expandirBtn.setIcon(
+                QtGui.QIcon(":/newPrefix/ICONS/multiple-file.png"))
 
 
 # _____________________________________________________#
--- animations_sbu.py
+++ animations_sbu.py
@@ -1,52 +1,36 @@
-
-from PyQt5 import QtGui
+from PyQt5 import QtGui, Qt
 import Source_rc
 from PyQt5.QtWidgets import QMainWindow
+
 
 #______ANIMATION CLASS THAT HAS THE BEHAVIOR OF THE OBJECTS_______#
 class Animations(QMainWindow):
 
     def toggle_left(self, animation, obj):
-        
+
         #calculate the end postition for the menu
         width = obj.width()
 
-        if width==0:
+        if width == 0:
             #If the menu es currently hidden, show it by moving it onto the screen
-            new_width=190
+            new_width = 190
             self.miempresaBtn.setIcon(
                 QtGui.QIcon(":/newPrefix/ICONS/angle-small-left (1).png"))
         else:
             #If the menu es currently visible, hide it by moving it off the left edge of the screen
-            new_width=0
+            new_width = 0
             self.miempresaBtn.setIcon(
                 QtGui.QIcon(":/newPrefix/ICONS/menu-hamburguesa.png"))
-        
+
         #set the start and end values for the animation
+
         animation.setStartValue(width)
         animation.setEndValue(new_width)
         animation.start()
-        
-    def toggle_right(self, animation1, animation2, obj1, obj2):
-        
-        width1=obj1.width()
-        width2=obj2.width()
- 
-        if (width1 and width2) ==0:
-            #If the widget is currently hidden, show it by moving it onto the screen
-            new_width1=350
-            new_width2=800
-            self.stackedWidget_2.setCurrentWidget(obj2)
-            self.stackedWidget.setCurrentWidget(obj1)
-        else:
-            #If the widget is currently visible, hide it by moving it off the left edge of the screen
-            new_width1=0
-            new_width2=0
-        
-        #set the start and end values for the animation
-        animation1.setStartValue(width1)
-        animation2.setStartValue(width2)
-        animation1.setEndValue(new_width1)
-        animation2.setEndValue(new_width2)
-        animation1.start()
-        animation2.start()
+
+    def toggle_right(self, obj1, obj2, mb_width, stack_widht):
+
+        obj1.setMaximumWidth(stack_widht)
+        obj2.setMaximumWidth(mb_width)
+        self.stackedWidget_2.setCurrentWidget(obj2)
+        self.stackedWidget.setCurrentWidget(obj1)
